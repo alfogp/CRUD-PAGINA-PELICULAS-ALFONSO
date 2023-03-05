9ad695d5ad28fe3f0f26d7f8261491bdbe7eb303
alfogp <117422698+alfogp@users.noreply.github.com>
2023-03-05 17:32:29 -0400

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php 
+
+function connection(){
+    $host ="localhost";
+    $user = "root";
+    $pass = "";
+    $bd = "user_crud_php";
+
+    $connect = mysqli_connect($host, $user, $pass);
+    mysqli_select_db($connect, $bd);
+
+    return $connect;
+};
+
+?>
